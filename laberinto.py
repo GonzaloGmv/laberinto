fc8306426534bf4a4066f4f95855a65855d98bde
GonzaloGmv <91721237+GonzaloGmv@users.noreply.github.com>
2021-11-28 17:21:17 +0100

    commit

---
+++ laberinto.py
@@ -0,0 +1,19 @@
+laberinto = [
+    [' ', 'X', 'X', 'X', 'X'], 
+    [' ', 'X', ' ', ' ', ' '],
+    [' ', 'X', ' ', 'X', ' '], 
+    [' ', ' ', ' ', 'X', ' '], 
+    ['X', 'X', 'X', 'X', 'S']
+]
+fila1 = laberinto[0]
+fila2 = laberinto[1]
+fila3 = laberinto[2]
+fila4 = laberinto[3]
+fila5 = laberinto[4]
+
+print("Laberinto:")
+print(fila1)
+print(fila2)
+print(fila3)
+print(fila4)
+print(fila5)
