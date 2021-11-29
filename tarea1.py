913d7a9a552ba587ba3241c4a3a0897e4c1aa752
GonzaloGmv <91721237+GonzaloGmv@users.noreply.github.com>
2021-11-29 18:22:36 +0100

    tarea1

--- tarea1.py
+++ tarea1.py
@@ -11,9 +11,5 @@ muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4
 for i in range (12):
     laberinto[muro[i][0]][muro[i][1]] = "X"
     
-
-print(laberinto[0])
-print(laberinto[1])
-print(laberinto[2])
-print(laberinto[3])
-print(laberinto[4])
+for i in range (5):
+    print(laberinto[i])
