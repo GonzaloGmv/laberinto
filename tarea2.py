a02472f0cfd7c654ef2e7befd9c7ae9a6db24d78
GonzaloGmv <91721237+GonzaloGmv@users.noreply.github.com>
2021-11-30 21:21:25 +0100

    commit

--- tarea2.py
+++ tarea2.py
@@ -10,13 +10,16 @@ laberinto = [
 tarea1.tablero(laberinto)
 i = 0
 j = 0
+k = 0
 while i < 5:
-    if laberinto[j][i+1] != 'X':
+    if laberinto[j][i+1] != 'X' and i < 4:
         print("Derecha")
         i = i + 1
-    elif laberinto [j+1][i] != 'X':
+    elif laberinto [j+1][i] != 'X' and j >= k:
         print("Abajo")
         j = j + 1
+        k = j - 1
     else:
         print("Arriba")
         j = j - 1
+        k = j + 1
