061fb1b6fdeac807c33fb91e20bab8df0ce8c0a7
GonzaloGmv <91721237+GonzaloGmv@users.noreply.github.com>
2021-12-01 20:04:18 +0100

    izquierda añadida y finalizado

--- tarea2.py
+++ tarea2.py
@@ -12,20 +12,29 @@ tarea1.tablero(laberinto)
 i = 0
 j = 0
 k = 0
+l = 0
 instrucciones = list('')
 
 while laberinto[i][j] != 'S':
-    if i < 4 and laberinto[j][i+1] != 'X':
+    if i < 4 and i >= k and laberinto[j][i+1] != 'X':
         instrucciones.append("Derecha")
         i = i + 1
-        k = j
-    elif j >= k and laberinto [j+1][i] != 'X':
+        k = i - 1
+        l = j
+    elif i > 0 and i < k and laberinto[j][i-1] != 'X':
+        instrucciones.append("Izquierda")
+        i = i - 1
+        k = i + i
+        l = j
+    elif j < 4 and j >= l and laberinto [j+1][i] != 'X':
         instrucciones.append("Abajo")
         j = j + 1
-        k = j - 1
+        l = j - 1
+        k = i
     else:
         instrucciones.append("Arriba")
         j = j - 1
-        k = j + 1
+        l = j + 1
+        k = i
 print("Asi es como se sale del laberinto: ")
 print(instrucciones)
